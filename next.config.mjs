8e1da078bf2d1f442bf08541aa66ee9f677d1841
abhinv-sabu16 <abhinavsabu22@gmail.com>
2026-03-17 10:10:28 +0530

    FIrst commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.notion.so', 'notion.so', 's3.us-west-2.amazonaws.com', 'images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: 'notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
   env: {
     NOTION_API_KEY: process.env.NOTION_API_KEY,
     NOTION_PROJECTS_DB_ID: process.env.NOTION_PROJECTS_DB_ID,
   },
+  serverExternalPackages: ['firebase-admin'],
 };
 
 export default nextConfig;
